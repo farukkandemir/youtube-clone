083ffd76942ecfb535cfe35c0e863e3312d28a5e
FARUK KANDEMIR <75193363+farukkandemir@users.noreply.github.com>
2022-09-10 06:51:25 -0400

    Update contants.js

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -37,5 +37,4 @@ export const categories = [
 export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
 export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
 export const demoVideoUrl = "/video/GDa8kZLNhJ4";
-export const demoChannelTitle = "JavaScript Mastery";
 
