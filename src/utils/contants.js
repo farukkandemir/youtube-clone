b4252d11b5eba7f426b71dd2b62a00db9a36547a
farukkandemir <farukkandemir09@gmail.com>
2022-10-16 17:59:54 -0400

    first commit

---
+++ src/utils/contants.js
@@ -0,0 +1,40 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "New", id: 1, icon: <HomeIcon /> },
+  { name: "BeardMeatsFood", id: 2, icon: <FastfoodIcon /> },
+  { name: "Coding", id: 3, icon: <CodeIcon /> },
+  { name: "ReactJS", id: 4, icon: <CodeIcon /> },
+  { name: "NextJS", id: 5, icon: <CodeIcon /> },
+  { name: "Music", id: 6, icon: <MusicNoteIcon /> },
+  { name: "Education", id: 7, icon: <SchoolIcon /> },
+  { name: "Podcast", id: 8, icon: <GraphicEqIcon /> },
+  { name: "Movie", id: 9, icon: <OndemandVideoIcon /> },
+  { name: "Gaming", id: 10, icon: <SportsEsportsIcon /> },
+  { name: "Live", id: 11, icon: <LiveTvIcon /> },
+  { name: "Sport", id: 12, icon: <FitnessCenterIcon /> },
+  { name: "Fashion", id: 13, icon: <CheckroomIcon /> },
+  { name: "Beauty", id: 14, icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", id: 15, icon: <TheaterComedyIcon /> },
+  { name: "Gym", id: 16, icon: <FitnessCenterIcon /> },
+  { name: "Crypto", id: 17, icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
+export const demoVideoUrl = "/video/GDa8kZLNhJ4";
+
